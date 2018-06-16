41df181f64d85fcbc1a67a7107803f0193c89587
eithuzartun <eithuzarhtun1996@gmail.com>
2018-06-16 21:36:12 +0630

    add ex47

---
+++ ex47/skeleton/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+        'description': 'My Projects',
+        'author': 'My Name',
+        'url': 'URL to get it at.',
+        'download_url': 'Where to download it.',
+        'author_email': 'fMy email',
+        'version': '0.1',
+        'install_requires': ['nose'],
+        'packages': ['ex47'],
+        'script': [ ],
+        'name': 'projectname'
+        }
+
+setup(**config)
