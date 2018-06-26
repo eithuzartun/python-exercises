417e279ad8485087b84598bd075a7c5ae55261ed
eithuzartun <eithuzarhtun1996@gmail.com>
2018-06-26 13:37:00 +0630

    add

---
+++ ex101.py
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'am split\non aline."
+backslash_cat = "I'am \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+    \t* Cat food
+    \t* Fishies
+    \t* Catnip\n\t* Grass
+    """
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
